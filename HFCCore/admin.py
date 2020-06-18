65999d0e95cfe50fa37932533d6e7b7d5e21e954
mohan_yerra <mohancyerra@gmail.com>
2020-06-19 02:19:28 +0530

    problem statement page changed and problem detail page added

--- HFCCore/admin.py
+++ HFCCore/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from .models import *
 from TFC.admin import Team_MemberInline
-from .models import Community_Organization, Project, Problem_Statement,Community_Member
-class Community_MemberInline(admin.TabularInline):
-	model = Community_Member
-	extra=0
+from .models import Community_Organization, Project, Problem_Statement, Community_Member
 
+class Problem_StatementInline(admin.TabularInline):
+	model = Problem_Statement
+	extra=0
 class PartnerAdmin(admin.ModelAdmin):
 	list_display = ('name','website','partner_desc','phone_number','email','logo','address','city','state','zip_code','subdomain','thankyou_template','upi_id')
-	inlines =[Team_MemberInline]
+	inlines =[Team_MemberInline, Problem_StatementInline]
 	class meta:
 		model=Partner
 
 class Problem_StatementAdmin(admin.ModelAdmin):
-	list_display = ('title', 'summary', 'background_info', 'related_link', 'proposed_solution', 'partner_id', 'status','issue_area')
+	list_display = ('title', 'summary', 'background_info', 'related_link', 'proposed_solution', 'partner_id', 'status')
+	def has_module_permission(self, request):
+		return False
 	class Meta:
 		model= Problem_Statement
 
@@ -22,16 +24,27 @@ class ProjectAdmin(admin.ModelAdmin):
 	class Meta:
 		model = Project
 
+class Community_MemberInline(admin.TabularInline):
+	model = Community_Member
+	extra=0
+
 class Community_OrganizationAdmin(admin.ModelAdmin):
 	list_display = ('organization_name', 'type', 'website', 'logo', 'coordinator_name', 'coordinator_email', 'coordinator_mobile')
-	inlines =[Community_MemberInline]
+	inlines = [Community_MemberInline]
 	class Meta:
 		model = Community_Organization
+
+class Community_MemberAdmin(admin.ModelAdmin):
+	list_display = ('name', 'email', 'level_of_expertise', 'areaofexpertise', 'type', 'coder_profile', 'linkedin_profile', 'organization_id')
+	def has_module_permission(self, request):
+		return False
+	class Meta:
+		model = Community_Member
+
 
 admin.site.register(Partner,PartnerAdmin)
 admin.site.register(Problem_Statement, Problem_StatementAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Community_Organization, Community_OrganizationAdmin)
-
-
+admin.site.register(Community_Member, Community_MemberAdmin)
 
--- HFCCore/views.py
+++ HFCCore/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.views import View
-
+from django.views import View, generic
+from .models import Problem_Statement
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request,'HFC/hfc_home.html')
+
+class ProblemStatementsView(generic.ListView):
+    def get(self, request):
+        problems_list = Problem_Statement.objects.all()
+        return render(request, 'HFC/problem_statements.html', {'problems_list': problems_list})
+
+class ProblemsWithIssueareaView(generic.ListView):
+    def get(self, request, issue_area):
+        problems_list = Problem_Statement.objects.filter(issue_area=issue_area)
+        return render(request, 'HFC/problem_statements.html', {'problems_list': problems_list})
+
+class ProblemDiscriptionView(View):
+    def get(self, request, title):
+        problem = Problem_Statement.objects.get(title=title)
+        return render(request, 'HFC/problem_discription.html', {'problem': problem})
