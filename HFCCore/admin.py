b83a22e8c0554b52b6a3efc739a498a3416c98e5
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2020-06-22 12:11:02 +0530

    Focus area added to Organization

--- HFCCore/admin.py
+++ HFCCore/admin.py
@@ -7,7 +7,7 @@ class Community_MemberInline(admin.TabularInline):
 	extra=0
 
 class PartnerAdmin(admin.ModelAdmin):
-	list_display = ('name','website','partner_desc','phone_number','email','logo','address','city','state','zip_code','subdomain','thankyou_template','upi_id')
+	list_display = ('name','website','partner_desc','phone_number','email','logo','address','city','state','zip_code','focus_area','subdomain','thankyou_template','upi_id')
 	inlines =[Team_MemberInline]
 	class meta:
 		model=Partner
--- TFC/admin.py
+++ TFC/admin.py
@@ -15,7 +15,7 @@ class Team_MemberInline(admin.TabularInline):
 	
 
 class OrganizationAdmin(admin.ModelAdmin):
-	list_display = ('name','website','partner_desc','phone_number','email','logo','address','city','state','zip_code','subdomain','thankyou_template','upi_id')
+	list_display = ('name','website','partner_desc','phone_number','email','logo','address','city','state','zip_code','focus_area','subdomain','thankyou_template','upi_id')
 	inlines =[Team_MemberInline]
 	class meta:
 		model=Organization
