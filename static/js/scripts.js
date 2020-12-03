802e1267bf4f361506f3ef60ae7642c5e6d1e61a
Srinivas Aki <srinivas.aki@gmail.com>
2020-12-03 12:37:18 +0530

    Moving local css, js to standard location

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,29 +1,37 @@
-$(document).ready(function(){
-	$('.menu-btn').click(function() {
+$(document).ready(function () {
+	$('.menu-btn').click(function () {
 		$(this).toggleClass('active');
 		$('.header__right').slideToggle();
 	});
 
-	if ( $('#datepicker').length > 0 ) {
-		$( "#datepicker" ).datepicker({
-		  dateFormat: "dd-mm-yy"
+	if ($('#datepicker').length > 0) {
+		$("#datepicker").datepicker({
+			dateFormat: "dd-mm-yy"
 		});
 	}
 
+	if (window.location.hash) {
+		if ($(window.location.hash).length > 0) {
+			$('.tab-sec li').removeClass('active');
+			$('a[href="' + window.location.hash + '"]').parent().addClass('active');
+
+			$('.hfc-outer section').removeClass('active');
+			$(window.location.hash).addClass('active');
+		}
+	}
+
 	function autoScroll() {
-	    $('.tab-sec a').on('click',function (e) {
-	        e.preventDefault();
+		$('.tab-sec a').on('click', function (e) {
+			e.preventDefault();
 
-	        var $el = $(this),
-	            id = $el.attr('href');
+			var $el = $(this),
+				id = $el.attr('href');
 
-	        $('.hfc-outer section').removeClass('active');
-	        $('.tab-sec li').removeClass('active');
-	        $(id).addClass('active');
-	        $(this).parent().addClass('active');
-	    });
+			$('.hfc-outer section').removeClass('active');
+			$('.tab-sec li').removeClass('active');
+			$(id).addClass('active');
+			$(this).parent().addClass('active');
+		});
 	}
 	autoScroll();
-
 });
-
