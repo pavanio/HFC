70f2087768d1b67df58448003a2f4f436b579bda
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2021-04-16 12:41:23 +0530

    HFCCore forms cleaned up

--- HFCCore/forms.py
+++ HFCCore/forms.py
@@ -17,119 +17,121 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class Mentor_form(forms.ModelForm):
-    field_order=['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
+    field_order = ['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
         'years_of_experience','profession','area_of_expertise']
     class Meta:
     	model  = Community_Member
-    	exclude=('level_of_expertise','organization_id','type')
+    	exclude = ('level_of_expertise','organization_id','type')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         #self.fields['gender']=forms.ChoiceField(choices=GENDER)
-        self.fields['highest_education']=forms.ChoiceField(choices=EDUCATION)
-        self.fields['years_of_experience']=forms.ChoiceField(choices=EXPERIENCE[4:])
+        self.fields['highest_education'] = forms.ChoiceField(choices = EDUCATION)
+        self.fields['years_of_experience'] = forms.ChoiceField(choices = EXPERIENCE[4:])
         self.fields['area_of_expertise'].widget = forms.CheckboxSelectMultiple()
-        self.fields['area_of_expertise'].queryset=Expertise.objects.none()
+        self.fields['area_of_expertise'].queryset = Expertise.objects.none()
         #self.fields['dob'] = forms.DateField(widget = DateInput)
         self.fields['profession'].empty_label = None
         if 'profession'in self.data:
             try:
                 expertise_area_id = int(self.data.get('profession'))
-                self.fields['area_of_expertise'].queryset =Expertise.objects.filter(category_of_expertise=expertise_area_id)
+                self.fields['area_of_expertise'].queryset = Expertise.objects.filter(category_of_expertise = expertise_area_id)
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk:
             self.fields['area_of_expertise'].queryset = self.instance.expertise_area.expertise_set
 
 class Center_contributor_form(forms.ModelForm):
-    field_order=['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
+    field_order = ['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
         'years_of_experience','organization_id','profession','area_of_expertise']
     class Meta:
     	model  = Community_Member
-    	exclude=('level_of_expertise','type')
+    	exclude = ('level_of_expertise','type')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.fields['gender']=forms.ChoiceField(choices=GENDER)
-        self.fields['highest_education']=forms.ChoiceField(choices=EDUCATION)
-        self.fields['years_of_experience']=forms.ChoiceField(choices=EXPERIENCE)
+        #self.fields['gender'] = forms.ChoiceField(choices=GENDER)
+        self.fields['highest_education'] = forms.ChoiceField(choices = EDUCATION)
+        self.fields['years_of_experience'] = forms.ChoiceField(choices = EXPERIENCE)
         self.fields['area_of_expertise'].widget = forms.CheckboxSelectMultiple()
-        self.fields['area_of_expertise'].queryset=Expertise.objects.none()
+        self.fields['area_of_expertise'].queryset = Expertise.objects.none()
         #self.fields['dob'] = forms.DateField(widget = DateInput)
         self.fields['profession'].empty_label = None
         if 'profession'in self.data:
             try:
                 expertise_area_id = int(self.data.get('profession'))
-                self.fields['area_of_expertise'].queryset =Expertise.objects.filter(category_of_expertise=expertise_area_id)
+                self.fields['area_of_expertise'].queryset = Expertise.objects.filter(category_of_expertise = expertise_area_id)
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk:
             self.fields['area_of_expertise'].queryset = self.instance.expertise_area.expertise_set
+
 class Chapter_contributor_form(forms.ModelForm):
-    field_order=['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
+    field_order = ['name','email','contact_number','dob','gender','highest_education','coder_profile','linkedin_profile',
         'years_of_experience','profession','area_of_expertise']
     class Meta:
     	model  = Community_Member
-    	exclude=('level_of_expertise','type','organization_id')
+    	exclude = ('level_of_expertise','type','organization_id')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.fields['gender']=forms.ChoiceField(choices=GENDER)
-        self.fields['highest_education']=forms.ChoiceField(choices=EDUCATION)
-        self.fields['years_of_experience']=forms.ChoiceField(choices=EXPERIENCE)
+        #self.fields['gender'] = forms.ChoiceField(choices=GENDER)
+        self.fields['highest_education'] = forms.ChoiceField(choices = EDUCATION)
+        self.fields['years_of_experience'] = forms.ChoiceField(choices = EXPERIENCE)
         self.fields['area_of_expertise'].widget = forms.CheckboxSelectMultiple()
-        self.fields['area_of_expertise'].queryset=Expertise.objects.none()
+        self.fields['area_of_expertise'].queryset = Expertise.objects.none()
         #self.fields['dob'] = forms.DateField(widget = DateInput)
         self.fields['profession'].empty_label = None
-        
         if 'profession'in self.data:
             try:
                 expertise_area_id = int(self.data.get('profession'))
-                self.fields['area_of_expertise'].queryset =Expertise.objects.filter(category_of_expertise=expertise_area_id)
+                self.fields['area_of_expertise'].queryset = Expertise.objects.filter(category_of_expertise = expertise_area_id)
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk:
             self.fields['area_of_expertise'].queryset = self.instance.expertise_area.expertise_set
+
 class Problem_Statement_form(forms.ModelForm):
-    issue_area = forms.ModelMultipleChoiceField(queryset=Issue_Area.objects.all(),
-    widget=forms.CheckboxSelectMultiple)
+    issue_area = forms.ModelMultipleChoiceField(queryset = Issue_Area.objects.all(),
+    widget = forms.CheckboxSelectMultiple)
     class Meta:
-        model=Problem_Statement
+        model = Problem_Statement
         fields = '__all__'
-        exclude=('status','partner_id')
+        exclude = ('status','partner_id')
         widgets = {
-          'summary': Textarea(attrs={'rows':5,'cols':15}),
-          'background_info':Textarea(attrs={'rows':5,'cols':15}),
-          'proposed_solution':Textarea(attrs={'rows':5,'cols':15}),
-          'related_link':Textarea(attrs={'rows':5,'cols':15}),
+          'summary': Textarea(attrs = {'rows':5,'cols':15}),
+          'background_info':Textarea(attrs = {'rows':5,'cols':15}),
+          'proposed_solution':Textarea(attrs = {'rows':5,'cols':15}),
+          'related_link':Textarea(attrs = {'rows':5,'cols':15}),
         }
 
 class Community_member_form(forms.ModelForm):
-    field_order=['name','email','contact_number','highest_education','city','linkedin_profile',
+    field_order = ['name','email','contact_number','highest_education','city','linkedin_profile',
         'years_of_experience','profession','area_of_expertise']
-    city = forms.ModelChoiceField(queryset = Community_Organization.objects.filter(type='Chapter').values_list('city',flat=True))
+    city = forms.ModelChoiceField(queryset = Community_Organization.objects.filter(type = 'Chapter').values_list('city',flat = True))
     class Meta:
     	model  = Community_Member
-    	exclude=('level_of_expertise','type','organization_id')
+    	exclude = ('level_of_expertise','type','organization_id')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.fields['gender']=forms.ChoiceField(choices=GENDER)
-        self.fields['highest_education']=forms.ChoiceField(choices=EDUCATION)
-        self.fields['years_of_experience']=forms.ChoiceField(choices=EXPERIENCE)
+        #self.fields['gender'] = forms.ChoiceField(choices=GENDER)
+        self.fields['highest_education'] = forms.ChoiceField(choices = EDUCATION)
+        self.fields['years_of_experience'] = forms.ChoiceField(choices = EXPERIENCE)
         self.fields['area_of_expertise'].widget = forms.CheckboxSelectMultiple()
-        self.fields['area_of_expertise'].queryset=Expertise.objects.none()
+        self.fields['area_of_expertise'].queryset = Expertise.objects.none()
         #self.fields['city'].queryset = Community_Organization.objects.filter('chapter').values_list('city',flat=True)
         self.fields['city'].empty_label = None
         self.fields['profession'].empty_label = None
         if 'profession'in self.data:
             try:
                 expertise_area_id = int(self.data.get('profession'))
-                self.fields['area_of_expertise'].queryset =Expertise.objects.filter(category_of_expertise=expertise_area_id)
+                self.fields['area_of_expertise'].queryset = Expertise.objects.filter(category_of_expertise = expertise_area_id)
             except (ValueError, TypeError):
                 pass
         elif self.instance.pk:
             self.fields['area_of_expertise'].queryset = self.instance.expertise_area.expertise_set
     
 class SendEmailForm(forms.Form):
-    subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': ('Subject'),'size': '60'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows':8, 'cols':60}))
-    users = forms.ModelMultipleChoiceField(label="To",
-                                           queryset=Community_Member.objects.all(),
-                                           widget=forms.SelectMultiple())
+    subject = forms.CharField(widget = forms.TextInput(attrs = {'placeholder': ('Subject'),'size': '60'}))
+    message = forms.CharField(widget = forms.Textarea(attrs = {'rows':8, 'cols':60}))
+    users = forms.ModelMultipleChoiceField(label = "To",
+                                           queryset = Community_Member.objects.all(),
+                                           widget = forms.SelectMultiple())
+    
