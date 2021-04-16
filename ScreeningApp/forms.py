3cf24d7eeac7c0eaaa37c7f7bb20ca50989ac453
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2021-04-16 12:49:21 +0530

    ScreeningApp forms file cleaned up

--- ScreeningApp/forms.py
+++ ScreeningApp/forms.py
@@ -1,29 +1,16 @@
 from ScreeningApp.models import *
 from django import forms
-
 from django.forms.widgets import RadioSelect
 
 class QuestionForm(forms.Form):
     def __init__(self,  *args, **kwargs):
-        self.screening_id=kwargs.pop('screening_id')
+        self.screening_id = kwargs.pop('screening_id')
         super(QuestionForm, self).__init__(*args, **kwargs)
         #choice_list = [x for x in question.get_questions()]
-        
-        questions=Screenings_Questions.objects.filter(screening_id=self.screening_id)
+        questions = Screenings_Questions.objects.filter(screening_id = self.screening_id)
         choice_list=[]
         for question in questions:
             for item in question.question.all():
                 pass
-                #print(choice_list.append(item.option_1))
-                #print(choice_list)
-                #choice_list.append(item.option_2)
-                #choice_list.append(item.option_3)
-                #choice_list.append(item.option_4)
-                                    
-        #self.fields["answer"] = forms.ChoiceField(choices=choice_list,
-                                                   #widget=RadioSelect)
         choice_list=[item.option_1 for item in question.question.all() for question in questions]
-        #print(choice_list)
         
-
-
