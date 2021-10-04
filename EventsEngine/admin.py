27e30ac6155dd95262e69d72240a8a71080d2841
YuZaGa <yumangr@gmail.com>
2021-10-04 18:43:03 +0530

    Summernote added to event Pages #232

--- EventsEngine/admin.py
+++ EventsEngine/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Events, EventType
 
-admin.site.register(Events)
+from django_summernote.admin import SummernoteModelAdmin
+
+
+class Events_Admin(SummernoteModelAdmin):
+	list_display = ( 'title','title_slug', 'start_date', 'end_date', 'description', 'agenda','status','email_confirmation' ,'registration' )
+	summernote_fields = ('description', 'agenda','email_confirmation')
+    
+	class meta:
+		model = Events
+
+admin.site.register(Events,Events_Admin)
 
 admin.site.register(EventType)
+
+
+  
