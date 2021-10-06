954e83968f5be08237cccdd9d5b5eb1ee5c6b27e
YuZaGa <yumangr@gmail.com>
2021-10-06 13:38:22 +0530

    Eevents Module UI Retouch

--- EventsEngine/urls.py
+++ EventsEngine/urls.py
@@ -10,9 +10,9 @@ handler500 = 'HFCCore.views.error_500'
 
 #app_name="EventsEngine"
 urlpatterns = [
-    path('ongoing-events/',views.EventList.as_view(),name='events'),
-    path('ongoing-events/<title_slug>',views.EventDetailView.as_view(),name = 'event_detail'),
-    path('ongoing-events/<title_slug>/signup',views.EventSignUpView.as_view(),name = 'event_sign_up'),
+    path('events/',views.EventList.as_view(),name='events'),
+    path('events/<title_slug>',views.EventDetailView.as_view(),name = 'event_detail'),
+    path('events/<title_slug>/signup',views.EventSignUpView.as_view(),name = 'event_sign_up'),
 
 
 ]
