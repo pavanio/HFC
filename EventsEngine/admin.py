7765b230e1f4a297fc81861e0e188b956b81920b
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2021-10-07 11:24:34 +0530

    Event Engine admin updated

--- EventsEngine/admin.py
+++ EventsEngine/admin.py
@@ -6,8 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 class Events_Admin(SummernoteModelAdmin):
-	list_display = ( 'title','title_slug', 'start_date', 'end_date', 'description', 'agenda','status','email_confirmation' ,'registration' )
+	list_display = ( 'title','title_slug', 'start_date', 'end_date', 'description', 'agenda','status','email_confirmation' , )
 	summernote_fields = ('description', 'agenda','email_confirmation')
+	readonly_fields = ('registration',)
     
 	class meta:
 		model = Events
