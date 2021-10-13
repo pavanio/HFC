e3bff8f8551254e35a9c1b3d8287dc0527d727cb
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2021-10-13 13:34:54 +0530

    Logo in problem statement list pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+from django import forms
+
+class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].required = True
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 class blogadmin(SummernoteModelAdmin):
     list_display = ('title', 'title_slug', 'status', 'created_on','image')
     summernote_fields = ('body', )
+    form = PostForm
 
 admin.site.register(Post, blogadmin)
+
+
