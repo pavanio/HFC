f027d914d3a30bae1914aae6d866f61a989ba218
YuZaGa <yumangr@gmail.com>
2021-11-01 12:22:33 +0530

    Community Page tabs #262

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,15 +10,8 @@ $(document).ready(function () {
 		});
 	}
 
-	if (window.location.hash) {
-		if ($(window.location.hash).length > 0) {
-			$('.tab-sec li').removeClass('active');
-			$('a[href="' + window.location.hash + '"]').parent().addClass('active');
 
-			$('.hfc-outer section').removeClass('active');
-			$(window.location.hash).addClass('active');
-		}
-	}
+	
 
 	function autoScroll() {
 		$('.tab-sec a').on('click', function (e) {
@@ -26,11 +19,24 @@ $(document).ready(function () {
 
 			var $el = $(this),
 				id = $el.attr('href');
-
+			
+				if (window.location.hash) {
+					if ($(window.location.hash).length > 0) {
+						$('.tab-sec li').removeClass('active');
+						$('a[href="' + window.location.hash + '"]').parent().addClass('active');
+						var hash = document.location.hash;
+						window.location.hash = id;
+						$('.hfc-outer section').removeClass('active');
+						$(window.location.hash).addClass('active');
+					}
+				}
+			
 			$('.hfc-outer section').removeClass('active');
 			$('.tab-sec li').removeClass('active');
 			$(id).addClass('active');
 			$(this).parent().addClass('active');
+
+
 		});
 	}
 	autoScroll();
